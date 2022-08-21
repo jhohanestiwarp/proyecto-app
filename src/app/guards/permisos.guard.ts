4ec728360cb5ad88d33515d2e94808568ef8cb00
Rafael Ramos <rafaelramosr@hotmail.com>
2022-08-21 18:57:04 -0500

    fix return check logging

--- src/app/guards/permisos.guard.ts
+++ src/app/guards/permisos.guard.ts
@@ -3,19 +3,16 @@ import {
   CanActivate,
   ActivatedRouteSnapshot,
   RouterStateSnapshot,
-  UrlTree,
   Router,
 } from '@angular/router';
-import { Observable } from 'rxjs';
 
-import { permisosService } from '../service/permisos.service';
 import { AuthResponse } from '../interfaces/login.interfaces';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CanActivateGuard implements CanActivate {
-  constructor(private router: Router, private auth: permisosService) {}
+  constructor(private router: Router) {}
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
@@ -23,15 +20,10 @@ export class CanActivateGuard implements CanActivate {
     return this.checkLogging(this.router.url);
   }
 
-  private checkLogging(url: string): Observable<boolean> {
-    return new Observable((obs) => {
-      const authResponse: string = localStorage.getItem('auth');
-      if (!authResponse) {
-        obs.next(false);
-        obs.complete();
-      }
-      return this.checkPermissions(JSON.parse(authResponse), url);
-    });
+  private checkLogging(url: string): boolean {
+    const authResponse: string = localStorage.getItem('auth');
+    if (!authResponse) return false;
+    return this.checkPermissions(JSON.parse(authResponse), url);
   }
 
   private checkPermissions(authData: AuthResponse, url: string): boolean {
